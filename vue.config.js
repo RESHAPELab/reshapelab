ce04610ef67360dd2228da3a17da8f34fae937ba
Igor Steinmacher <igor.steinmacher@nau.edu>
2024-03-29 00:00:30 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: ' '
   outputDir: 'dist',
   assetsDir: 'assets',
   indexPath: 'index.html',
